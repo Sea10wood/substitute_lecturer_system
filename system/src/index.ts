38734cb2449a0a20264b98c51cd3eed0f6938ec5
Sea10wood <miyusea5@gmail.com>
2023-10-21 16:58:08 +0900

    feature_setup

---
+++ system/src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('*', (c) => c.text('Hello World!'));
+
+app.post('/api/webhook', async (c) => {
+	console.log(JSON.stringify(c));
+	return c.json({ message: 'Hello World!' });
+});
+
+export default app;
